9988f47fcc8bcd5c557e33c2ac20268aefd085bc
Miqueas <proyectosmiqueas@gmail.com>
2026-01-05 02:38:38 -0300

    changed README

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -26,39 +26,41 @@ const SearchResults = () => {
       <SearchBar />
       <BreadCrumb categories={categories} />
       {filteredProducts.length === 0 ? (
-        <div className='no-results'>No se encontraron productos relacionados a <b>"{query}"</b></div>
-      )
-      :
-      filteredProducts.map((product) => (
-        <div key={product.id} className="containerProducts">
-          <button
-            onClick={() => navigate(`/items/${product.id}`)}
-            className="buttons"
-          >
-            <img
-              className="thumbnail"
-              src={product.thumbnail}
-              alt="thumbnail"
-            />
-            <div className="container-price-shipping-title">
-              <div className="price-shipping">
-                <p className="price">
-                  $ {product.price.toLocaleString('es-AR')}
-                </p>
-                {product.shipping.free_shipping === true && (
-                  <img
-                    className="shipping"
-                    src={freeShipping}
-                    alt="free_shipping"
-                  />
-                )}
-              </div>
-              <p className="title">{product.title}</p>
-            </div>
-            <p className="state">{product.address.state_name}</p>
-          </button>
+        <div className="no-results">
+          No se encontraron productos relacionados a <b>"{query}"</b>
         </div>
-      ))}
+      ) : (
+        filteredProducts.map((product) => (
+          <div key={product.id} className="containerProducts">
+            <button
+              onClick={() => navigate(`/items/${product.id}`)}
+              className="buttons"
+            >
+              <img
+                className="thumbnail"
+                src={product.thumbnail}
+                alt="thumbnail"
+              />
+              <div className="container-price-shipping-title">
+                <div className="price-shipping">
+                  <p className="price">
+                    $ {product.price.toLocaleString('es-AR')}
+                  </p>
+                  {product.shipping.free_shipping === true && (
+                    <img
+                      className="shipping"
+                      src={freeShipping}
+                      alt="free_shipping"
+                    />
+                  )}
+                </div>
+                <p className="title">{product.title}</p>
+              </div>
+              <p className="state">{product.address.state_name}</p>
+            </button>
+          </div>
+        ))
+      )}
     </div>
   )
 }
